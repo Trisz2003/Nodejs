479296bd4ec6ef4631ff679a57cab37bb9ce3066
Trisz2003 <nquangtri2003@gmail.com>
2024-08-16 11:31:24 +0700

    remove public/css

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').create({
+
+  defaultLayout: 'main',
+  
+  extname: '.hbs'
+  
+  });
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+//http loger
+app.use(morgan('combined'))
+//template engine
+app.engine('hbs', handlebars.engine);
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
