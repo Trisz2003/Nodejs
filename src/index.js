b66c4fd3609596243fa62d31157b0eb0b79cc37c
Trisz2003 <nquangtri2003@gmail.com>
2024-08-19 22:22:26 +0700

    Refacter code: creat routes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,38 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').create({
-
+  
   defaultLayout: 'main',
   
   extname: '.hbs'
   
-  });
+});
+const route = require('./routes')
+
+
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //http loger
-app.use(morgan('combined'))
+// app.use(morgan('combined'));
 //template engine
 app.engine('hbs', handlebars.engine);
-
+!
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
+
+
+//route
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/news.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const router = express.Router();
+
+const newsController = require('../app/controllers/newsController')
+
+
+router.use('/:slug',newsController.show);
+router.use('/',newsController.index);
+
+module.exports = router;
